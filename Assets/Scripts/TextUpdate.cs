8d2e2602ecc5726c89598d76db3a9552aa97a3ec
wilbefast <wilbefast@gmail.com>
2013-02-10 20:52:35 +0100

    gui text working, displaying joint angle in degrees (Eulerian)

--- Assets/Scripts/TextUpdate.cs
+++ Assets/Scripts/TextUpdate.cs
@@ -3,17 +3,36 @@ using System.Collections;
 
 public class TextUpdate : MonoBehaviour 
 {
+	//! --------------------------------------------------------------------------
+	//! ATTRIBUTES
+	//! --------------------------------------------------------------------------
+	// Parameterable
 	public GUIText gui;
+	
+	// Alias
+	private ZigSkeleton skeleton;	
+	
+	//! --------------------------------------------------------------------------
+	//! UNITY CALLBACKS
+	//! --------------------------------------------------------------------------
 	
 	// Use this for initialization
 	void Start () 
 	{
-	
+		skeleton = (ZigSkeleton)gameObject.transform.parent.FindChild("Blockman")
+																			.gameObject.GetComponent("ZigSkeleton");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		gui.text = "Hello World!";
+		// gui.text = skeleton.RightWrist.rotation.ToString ();
+		gui.text = skeleton.RightShoulder.rotation.eulerAngles.ToString ();
 	}
+	
+	//! --------------------------------------------------------------------------
+	//! OTHER METHODS
+	//! --------------------------------------------------------------------------
+	
+	
 }
