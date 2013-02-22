39d18ed419c85bb3e39d25c1b9194e2916e49210
geoffrey <geoffrey.melia@gmail.com>
2013-02-22 12:22:14 +0100

    test_dorsiflexion

--- Assets/Scripts/DorsiflexionMonitor.cs
+++ Assets/Scripts/DorsiflexionMonitor.cs
@@ -1,47 +1,68 @@
 using UnityEngine;
 using System.Collections;
 
-public class DorsiflexionMonitor : MonoBehaviour
+public class DorsiflexionMonitor : ExerciseMonitor
 {
 	//! --------------------------------------------------------------------------
 	//! ATTRIBUTES
 	//! --------------------------------------------------------------------------
 	// Parameters
-	public Grapher bendGraph, angleGraph;
-	public ZigSkeleton skeleton;
-	
+	public FailGrapher graph_wrist;
+	//public float max_forward_turn, max_elbow_bend, max_start_elevation;
 	// Local variables
-	private Vector3 shoulder, elbow, wrist, upperarm, forearm, spine_up;
+	private Vector3 wrist, fingertip;
+	private bool cheating = false;
 	
 	//! --------------------------------------------------------------------------
 	//! CALLBACKS
 	//! --------------------------------------------------------------------------
 	
-	// Use this for initialization
-	void Start ()
-	{
-	}
-	
 	// Update is called once per frame
 	void Update ()
 	{
-		// Reset local variables -- NB: '=' operator performs a deep copy
-		shoulder = skeleton.RightShoulder.position;
-		elbow = skeleton.RightElbow.position;
-		wrist = skeleton.RightWrist.position;
-		upperarm = elbow - shoulder;
-		forearm = wrist - elbow;
-		
-		// How straight is the arm ?
-		float bend = Vector3.Angle(upperarm, forearm);
-		
-		// How high has the arm been raised
-		float elevation = 180 - Vector3.Angle(skeleton.Torso.up, upperarm);
-		
-		// plot the data
-		if(bendGraph != null)
-			bendGraph.newDataPoint(bend);
-		if(angleGraph != null)
-			angleGraph.newDataPoint(elevation);
+		if(state != State.NO_USER)
+		{
+			// get new joint positions
+			reset_joint_position();
+				
+			// constraints
+			cheating = false;
+			// these two can reset 'cheating' to 'true' if applicable
+			wrist_bend();
+			
+			// progress 
+			//monitor_elevation(cheating);
+		}
+	}
+	
+	
+	//! --------------------------------------------------------------------------
+	//! SUBROUTINES -- ROTATION MEASUREMENTS
+	//! --------------------------------------------------------------------------
+	
+	private void reset_joint_position()
+	{
+		// reset local variables -- NB: '=' operator performs a deep copy
+		wrist = skeleton.RightHand.position;
+		fingertip = skeleton.RightFingertip.position;
+	}
+	
+	
+	private void wrist_bend()
+	{
+		// how straight is the arm ?
+		float wrist_bend = Vector3.Angle(wrist, fingertip);
+		/*if(elbow_bend > max_elbow_bend)
+		{
+			graph_elbow.setFail(true);
+			failedExercise();
+			cheating = true;
+		}
+		else
+			graph_elbow.setFail(false);
+		*/
+		// plot constraint data
+		if(graph_wrist != null)
+			graph_wrist.newDataPoint(wrist_bend);
 	}
 }
