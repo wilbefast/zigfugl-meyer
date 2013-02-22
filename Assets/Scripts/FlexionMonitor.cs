028aa21e359cd5a672f67a29ba8ae532102d3487
wilbefast <wilbefast@gmail.com>
2013-02-22 12:43:36 +0100

    fixed bug which occured because class names in FlexionMonitor.cs and AbductionMonitor.cs were reversed

--- Assets/Scripts/FlexionMonitor.cs
+++ Assets/Scripts/FlexionMonitor.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class AbductionMonitor : ExerciseMonitor
+public class FlexionMonitor : ExerciseMonitor
 {
 	//! --------------------------------------------------------------------------
 	//! ATTRIBUTES
